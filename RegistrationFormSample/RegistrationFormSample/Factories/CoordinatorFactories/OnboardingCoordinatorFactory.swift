ab8562444d7103261dde26ee4090caf26f2b2779
Serhii Palash <serhii.palash@brainbeanapps.com>
2020-03-07 11:35:49 +0200

    Initial commit

---
+++ RegistrationFormSample/RegistrationFormSample/Factories/CoordinatorFactories/OnboardingCoordinatorFactory.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingCoordinatorFactory.swift
+//  RegistrationFormSample
+//
+//  Created by Serhii Palash on 07/03/2020.
+//  Copyright © 2020 Serhii Palash. All rights reserved.
+//
+
+import Foundation
+
+class OnboardingCoordinatorFactory: ICoordinatorFactory {
+    func create(router: Routable, services: IAppServices) -> Coordinatable & CoordinatorFinishable {
+        return OnboardingCoordinator(router: router, applicationServices: services)
+    }
+}
